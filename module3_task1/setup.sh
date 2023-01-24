2d6dea7cfd0c337bfbd6f71d69d28d3a8f84261c
Brandon <brandon_scott585@yahoo.com>
2023-01-24 17:27:18 -0600

    makefile changes

--- module3_task1/setup.sh
+++ module3_task1/setup.sh
@@ -4,3 +4,4 @@ apt-get install curl -y
 curl -Lo install_hugo.deb https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_extended_0.109.0_linux-amd64.deb
 apt-get install ./install_hugo.deb
 apt-get install shellcheck -y
+make run
