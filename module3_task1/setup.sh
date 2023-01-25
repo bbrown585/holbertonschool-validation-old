3438e94de5d961af92a89055aee33301586fd55f
Brandon <brandon_scott585@yahoo.com>
2023-01-25 13:44:22 -0600

    setup.sh

--- module3_task1/setup.sh
+++ module3_task1/setup.sh
@@ -1,1 +1,8 @@
-ls -l
+#!/bin/bash
+apt update
+apt upgrade
+apt install -y wget
+apt install -y make
+wget https://github.com/gohugoio/hugo/releases/download/v0.84.0/hugo_extended_0.84.0_Linux-64bit.deb
+sudo dpkg -i hugo_extended_0.84.0_Linux-64bit.deb
+make build
