03c3791255d9142800008cf456f8de7c7bd78425
Brandon <brandon_scott585@yahoo.com>
2023-01-26 09:57:57 -0600

    chmod

--- module3_task1/setup.sh
+++ module3_task1/setup.sh
@@ -2,5 +2,4 @@
 apt-get update && apt-get install -y make golang-go git curl
 curl -L https://github.com/gohugoio/hugo/releases/download/v0.84.0/hugo_0.84.0_Linux-64bit.deb -o hugo.deb
 apt install ./hugo.deb
-make build
 apt-get install -y shellcheck 
