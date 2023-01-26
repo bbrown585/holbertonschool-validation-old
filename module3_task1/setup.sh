044cebe0c98435c96426708b2001d689139a3f58
bbrown585 <brandon_scott585@yahoo.com>
2023-01-26 19:33:43 +0000

    add files

--- module3_task1/setup.sh
+++ module3_task1/setup.sh
@@ -6,3 +6,4 @@ apt-get install -y shellcheck
 wget https://dl.google.com/go/go1.19.5.linux-amd64.tar.gz
 rm -rf /usr/local/go && tar -C /usr/local -xzf go1.19.5.linux-amd64.tar.gz
 export PATH=/usr/local/go/bin:$PATH
+
