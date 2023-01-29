5d9188f38d8d76e2407e9216cc4a2e1740a9061e
Brandon <brandon_scott585@yahoo.com>
2023-01-29 11:38:55 -0600

    workflow

--- module3_task4/setup.sh
+++ module3_task4/setup.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
-apt-get update && apt-get install -y hugo make
-apt-get install curl -y
-curl -Lo install_hugo.deb https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_extended_0.109.0_linux-amd64.deb
-apt-get install ./install_hugo.deb
-npm install -g markdownlint-cli
-rm ./install_hugo.deb
-sudo apt install zip
-rm -rf /usr/local/go && tar -C /usr/local -xzf go1.19.5.linux-amd64.tar.gz
-export PATH=/usr/local/go/bin:$PATH
+
+# 1
+apt-get update && apt-get install -y wget make
+apt-get install markdown -y
+apt-get install zip -y
+#npm install markdownlint --save-dev
+npm install markdownlint-cli -g -y
+wget https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_extended_0.109.0_Linux-64bit.tar.gz
+tar -xvf hugo_extended_0.109.0_Linux-64bit.tar.gz hugo
+mv hugo /usr/local/bin/
+rm hugo_extended_0.109.0_Linux-64bit.tar.gz
+
+# 2
+make build
