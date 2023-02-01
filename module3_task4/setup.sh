c023386535f43a0e4280c5bc34ce58c806d8fc4e
Brandon Brown <101846323+bbrown585@users.noreply.github.com>
2023-01-31 21:26:54 -0600

    Update setup.sh

--- module3_task4/setup.sh
+++ module3_task4/setup.sh
@@ -13,7 +13,6 @@ npm install @colors/colors @gar/promisify -g -y
 npm install npm@latest -g -y
 npm install -g npm@9.4.0
 npm install -g markdownlint-cli -g
-rm ./install_hugo.deb
 sudo apt install zip
 rm -rf /usr/local/go && tar -C /usr/local -xzf go1.19.5.linux-amd64.tar.gz
 export PATH=/usr/local/go/bin:$PATH
