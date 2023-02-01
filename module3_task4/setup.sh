80afea399e065e04ff32d40d297e2940ec61c2d3
bbrown585 <brandon_scott585@yahoo.com>
2023-02-01 00:34:22 +0000

    setup

--- module3_task4/setup.sh
+++ module3_task4/setup.sh
@@ -3,7 +3,8 @@ apt-get update && apt-get install -y hugo make
 apt-get install curl -y
 curl -Lo install_hugo.deb https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_extended_0.109.0_linux-amd64.deb
 apt-get install ./install_hugo.deb
-npm install -g markdownlint-cli
+npm install @colors/colors @gar/promisify -g -y
+npm install npm@latest -g -y
 rm ./install_hugo.deb
 sudo apt install zip
 rm -rf /usr/local/go && tar -C /usr/local -xzf go1.19.5.linux-amd64.tar.gz
