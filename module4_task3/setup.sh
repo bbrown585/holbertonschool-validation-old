37343f5048448a7d62f846c977768d0c00b09465
Brandon Brown <101846323+bbrown585@users.noreply.github.com>
2023-02-19 15:41:54 -0600

    Update setup.sh

--- module4_task3/setup.sh
+++ module4_task3/setup.sh
@@ -1,2 +1,15 @@
-#!/usr/bin/env bash
-apt-get update && apt-get install -y make
+#!/bin/bash
+apt-get update && apt-get install curl make npm
+npm install -g n
+n 16.0.0
+hash -r
+npm install -g markdownlint-cli -g
+brew install hadolint
+brew install container-structure-test
+npm install netlify-cli -g
+apt-get install curl -y
+npm install npm@latest -g -y
+npm install -g npm@9.4.0
+sudo apt install zip
+rm -rf /usr/local/go && tar -C /usr/local -xzf go1.19.5.linux-amd64.tar.gz
+export PATH=/usr/local/go/bin:$PATH
